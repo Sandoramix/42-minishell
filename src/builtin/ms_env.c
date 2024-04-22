09251dac7b6158ec83808f25201d59d1311cb0ff
marboccu <marboccu@student.42fi.it>
2024-04-22 12:08:08 +0200

    [CMD] env Fixes #8 + fix exit on stderr

---
+++ src/builtin/ms_env.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_env.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marboccu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 11:44:22 by marboccu          #+#    #+#             */
+/*   Updated: 2024/04/22 12:07:32 by marboccu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+void ms_env(t_var *mshell)
+{
+	int i;
+	t_list *env;
+
+	i = 0;
+	env = mshell->env;
+	while (env)
+	{
+		printf("%s=%s\n", (char *)env->key, (char *)env->val);
+		env = env->next;
+	}
+}
+
+//int	main(int ac, char **av, char **envp)
+//{
+//	t_var			mshell;
+
+//	(void)av;
+//	(void)ac;
+//	mshell = (t_var){0};
+//	if (ac != 1)
+//		return (pf_errcode(ERR_INVALID_ARGC), cleanup(&mshell, true, 1), 1);
+//	mshell._main.envp = envp;
+//	ms_init(&mshell);
+//	//ms_prompt(&mshell);
+//	ms_env(&mshell);
+//	return (cleanup(&mshell, true, 0));
+//}
