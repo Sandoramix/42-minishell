26da1bc0956f5e3c01e37ef399f381a200f31db7
odudniak <odudniak@student.42firenze.it>
2024-05-04 20:10:33 +0200

    [fix] + added malloc guards

--- src/libftx/src/sys/cmd_parseargs.c
+++ src/libftx/src/sys/cmd_parseargs.c
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 13:16:03 by odudniak          #+#    #+#             */
-/*   Updated: 2024/05/04 16:56:21 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/05/04 20:09:29 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,8 @@ char	**cmd_parse(char *raw)
 				dbg_printf("\tIt's near the char %c\n", raw[i - 1]);
 				res[residx] = str_freejoin(res[residx], tmp);
 				free(tmp);
+				if (!res[residx])
+					return (str_freemtx(res), NULL);
 			}
 			else if (!str_mtxpush(&res, tmp) || ++residx < -1)
 				return (str_freemtx(res), free(tmp), NULL);
@@ -92,6 +94,7 @@ t_list	*cmd_parse_new(char *raw)
 	res = NULL;
 	i = -1;
 	edgeidx = 0;
+	dbg_printf(COLOR_RED"[cmd_parse] input: `%s`\n"CR, raw);
 	while (raw && raw[++i])
 	{
 		if (chr_isquote(raw[i]))
@@ -107,14 +110,19 @@ t_list	*cmd_parse_new(char *raw)
 				return (lst_free(&res, free), NULL);
 			if (i != 0 && !ft_isspace(raw[i - 1]) && !chr_istoken(raw[i - 1]))
 			{
-				dbg_printf("\tIt's near the char %c\n", raw[i - 1]);
 				last = lst_gettail(res);
-				if (!last && !lst_addnew_tail(&res, tmp, (void *)A_DEFAULT))
+				dbg_printf("\tIt's near the char %c\n", raw[i - 1]);
+				if (!last && !lst_addnew_tail(&res, tmp, NULL))
 					return (lst_free(&res, free), free(tmp), NULL);
 				else
+				{
 					last->val = str_freejoin(last->val, tmp);
+					free(tmp);
+					if (!last->val)
+						return (lst_free(&res, free), NULL);
+				}
 			}
-			else if (!lst_addnew_tail(&res, tmp, (void *)A_DEFAULT))
+			else if (!lst_addnew_tail(&res, tmp, NULL))
 				return (lst_free(&res, free), free(tmp), NULL);
 			if (edgeidx - 1 == i)
 			{
@@ -134,7 +142,7 @@ t_list	*cmd_parse_new(char *raw)
 			tmp = my_substr(raw, edgeidx, i);
 			if (!tmp)
 				return (lst_free(&res, free), NULL);
-			else if (!lst_addnew_tail(&res, tmp, (void *)A_DEFAULT)) // todo TOKEN not to free.
+			else if (!lst_addnew_tail(&res, tmp, NULL))
 				return (lst_free(&res, free), free(tmp), NULL);
 		}
 		else if ((!ft_isspace(raw[i]) && !chr_istoken(raw[i])) && (ft_isspace(raw[i + 1]) || !raw[i + 1] || chr_isquote(raw[i + 1]) || chr_istoken(raw[i + 1])))
@@ -145,16 +153,21 @@ t_list	*cmd_parse_new(char *raw)
 			tmp = my_substr(raw, edgeidx, i);
 			if (!tmp)
 				return (lst_free(&res, free), NULL);
-			if (i != 0 && !ft_isspace(raw[edgeidx - 1]) && !chr_istoken(raw[i - 1]))
+			if (i != 0 && edgeidx != 0 && !ft_isspace(raw[edgeidx - 1]) && !chr_istoken(raw[edgeidx - 1]))
 			{
-				dbg_printf("\tIt's near the char %c\n", raw[i - 1]);
+				dbg_printf("\tIt's near the char %c\n", raw[edgeidx - 1]);
 				last = lst_gettail(res);
-				if (!last && !lst_addnew_tail(&res, tmp, (void *)A_DEFAULT))
+				if (!last && !lst_addnew_tail(&res, tmp, NULL))
 					return (lst_free(&res, free), free(tmp), NULL);
 				else
+				{
 					last->val = str_freejoin(last->val, tmp);
+					free(tmp);
+					if (!last->val)
+						return (lst_free(&res, free), NULL);
+				}
 			}
-			else if (!lst_addnew_tail(&res, tmp, (void *)A_DEFAULT))
+			else if (!lst_addnew_tail(&res, tmp, NULL))
 				return (lst_free(&res, free), free(tmp), NULL);
 			edgeidx = i + 1;
 		}
