ae5849edbd8bb08038ab5aeea30818a003788005
Sandoramix <sandoramix.dev@gmail.com>
2024-05-05 15:22:24 +0200

    updated test main & arg start from the second element (WIP: args needs to be converted into the new list)

--- src/builtin/ms_unset.c
+++ src/builtin/ms_unset.c
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 14:25:21 by odudniak          #+#    #+#             */
-/*   Updated: 2024/04/24 15:37:17 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/05/05 14:32:10 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void	ms_unset_debug(t_var *mshell, char **args, char *stage)
 	int		i;
 	t_list	*node;
 
-	i = -1;
+	i = 0;
 	if (!DEBUG)
 		return ;
 	printf(COLOR_MAGENTA"[ms_unset] %s\n"CR, stage);
@@ -36,26 +36,33 @@ void	ms_unset(t_var *mshell, char **args)
 {
 	int	i;
 
-	i = -1;
+	i = 0;
 	ms_unset_debug(mshell, args, "PRE UNSET");
 	while (args && args[++i])
 		lst_delbykey(&mshell->env, args[i], (void *)str_equals, free);
 	ms_unset_debug(mshell, args, "POST UNSET");
 }
 
-/*
-int	main(int ac, char **av, char **envp)
-{
-	const char		*unset_args[] = {"HOME", "USER", "PATH", "CWD", NULL};
-	t_var			mshell;
+//int	main(int ac, char **av, char **envp)
+//{
+//	t_var			mshell;
+//	char			**args;
+//	t_list			*listargs;
 
-	(void)av;
-	mshell = (t_var){0};
-	if (ac != 1)
-		return (pf_errcode(ERR_INVALID_ARGC), cleanup(&mshell, true, 1), 1);
-	mshell._main.envp = envp;
-	ms_init(&mshell);
-	ms_unset(&mshell, (char **)unset_args);
-	return (cleanup(&mshell, true, 0));
-}
-*/
+//	(void)av;
+//	mshell = (t_var){0};
+//	if (ac != 1)
+//		return (pf_errcode(ERR_INVALID_ARGC), cleanup(&mshell, true, 1), 1);
+//	mshell._main.envp = envp;
+//	ms_init(&mshell);
+//	args = cmd_parse("''un\"set\" LESS ''  'P''ATH' 'HOME' 'LS_COLORS'");
+//	args = expand_and_clear_mtx(args);
+//	ms_unset(&mshell, args);
+//	str_freemtx(args);
+
+//	listargs = cmd_parse_new("export");
+//	listargs = expand_and_clear(listargs);
+//	ms_export(&mshell, listargs);
+//	lst_free(&listargs, free);
+//	return (cleanup(&mshell, true, 0));
+//}
