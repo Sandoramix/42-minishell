d68a204ebcfaa6a4195816efc94f197b98859887
odudniak <odudniak@student.42firenze.it>
2024-05-08 16:08:03 +0200

    [refactor: wip]

--- src/parsing/cmd_parse_new.c
+++ src/parsing/cmd_parse_new.c
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 13:16:03 by odudniak          #+#    #+#             */
-/*   Updated: 2024/05/05 14:17:22 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/05/08 15:06:12 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,23 @@ static void	print_fromto(char *s, int start, int end)
 	write(1, "[", 1);
 	write(1, s + start, end - start + 1);
 	write(1, "]", 1);
+}
+
+static bool	concat_arg(t_list **res, char *concat)
+{
+	t_list	*last;
+
+	last = lst_gettail(*res);
+	if (!last && !lst_addnew_tail(res, concat, NULL))
+		return (lst_free(res, free), free(concat), false);
+	else
+	{
+		last->val = str_freejoin(last->val, concat);
+		free(concat);
+		if (!last->val)
+			return (lst_free(res, free), false);
+	}
+	return (true);
 }
 
 t_list	*cmd_parse_new(char *raw)
@@ -36,7 +53,9 @@ t_list	*cmd_parse_new(char *raw)
 	dbg_printf(COLOR_RED"[cmd_parse] input: `%s`\n"CR, raw);
 	while (raw && raw[++i])
 	{
-		if (chr_isquote(raw[i]))
+		if (ft_isspace(raw[i]) && !ft_isspace(raw[i + 1]))
+			edgeidx = i + 1;
+		else if (chr_isquote(raw[i]))
 		{
 			edgeidx = chr_quoteclose_idx(raw, raw[i], i);
 			dbg_printf("Found a quote {%c} at:\t[%3d] - [%3d]:\t", raw[i], i, edgeidx);
@@ -51,15 +70,8 @@ t_list	*cmd_parse_new(char *raw)
 			{
 				last = lst_gettail(res);
 				dbg_printf("\tIt's near the char %c\n", raw[i - 1]);
-				if (!last && !lst_addnew_tail(&res, tmp, NULL))
-					return (lst_free(&res, free), free(tmp), NULL);
-				else
-				{
-					last->val = str_freejoin(last->val, tmp);
-					free(tmp);
-					if (!last->val)
-						return (lst_free(&res, free), NULL);
-				}
+				if (!concat_arg(&res, tmp))
+					return (NULL);
 			}
 			else if (!lst_addnew_tail(&res, tmp, NULL))
 				return (lst_free(&res, free), free(tmp), NULL);
@@ -71,8 +83,6 @@ t_list	*cmd_parse_new(char *raw)
 			else
 				i = edgeidx;
 		}
-		else if (ft_isspace(raw[i]) && !ft_isspace(raw[i + 1]))
-			edgeidx = i + 1;
 		else if (chr_istoken(raw[i]) && (ft_isspace(raw[i + 1]) || !raw[i + 1] || chr_isquote(raw[i + 1])))
 		{
 			dbg_printf("Found unquoted TOKEN at:\t[%3d] - [%3d]:\t", edgeidx, i);
@@ -95,16 +105,8 @@ t_list	*cmd_parse_new(char *raw)
 			if (i != 0 && edgeidx != 0 && !ft_isspace(raw[edgeidx - 1]) && !chr_istoken(raw[edgeidx - 1]))
 			{
 				dbg_printf("\tIt's near the char %c\n", raw[edgeidx - 1]);
-				last = lst_gettail(res);
-				if (!last && !lst_addnew_tail(&res, tmp, NULL))
-					return (lst_free(&res, free), free(tmp), NULL);
-				else
-				{
-					last->val = str_freejoin(last->val, tmp);
-					free(tmp);
-					if (!last->val)
-						return (lst_free(&res, free), NULL);
-				}
+				if (!concat_arg(&res, tmp))
+					return (NULL);
 			}
 			else if (!lst_addnew_tail(&res, tmp, NULL))
 				return (lst_free(&res, free), free(tmp), NULL);
