9ccc0123874d9febe37b63cadec50533b51acb8c
odudniak <odudniak@student.42firenze.it>
2024-05-08 17:52:08 +0200

    [refactor: wip]  parsing

--- src/parsing/cmd_parse_new.c
+++ src/parsing/cmd_parse_new.c
@@ -6,43 +6,83 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 13:16:03 by odudniak          #+#    #+#             */
-/*   Updated: 2024/05/08 15:06:12 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/05/08 17:51:24 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-// TODO: add me to libftx
-static void	print_fromto(char *s, int start, int end)
+typedef enum e_cmd_parseswitch
 {
-	if (end < start)
+	CMDP_SPACE,
+	CMDP_QUOTE,
+	CMDP_TOKEN,
+	CMDP_WORD
+}	t_cmd_parseswitch;
+
+// TODO: add me to libftx
+static void	cmd_dbgparse(t_cmd_parseswitch type, char *s, int i, int edge)
+{
+	if (type == CMDP_QUOTE)
+		dbg_printf("Found a quote {%c} at:\t[%3d] - [%3d]:\t", s[i], i, edge);
+	else if (type == CMDP_TOKEN || type == CMDP_WORD)
+		dbg_printf("Found unquoted %s at:\t[%3d] - [%3d]:\t",
+			(char *[2]){"TOKEN", "WORD"}[type == CMDP_WORD], edge, i);
+	else
 		return ;
 	write(1, "[", 1);
-	write(1, s + start, end - start + 1);
-	write(1, "]", 1);
+	if (type == CMDP_QUOTE)
+		write(1, s + i, edge - i + 1);
+	else
+		write(1, s + edge, i - edge + 1);
+	write(1, "]\n", 2);
 }
 
-static bool	concat_arg(t_list **res, char *concat)
+static bool	lst_strappend_last(t_list **res, char *append, char dbg_char)
 {
 	t_list	*last;
 
+	dbg_printf("\tIt's near the char %c\n", dbg_char);
 	last = lst_gettail(*res);
-	if (!last && !lst_addnew_tail(res, concat, NULL))
-		return (lst_free(res, free), free(concat), false);
+	if (!last && !lst_addnew_tail(res, append, NULL))
+		return (lst_free(res, free), free(append), false);
 	else
 	{
-		last->val = str_freejoin(last->val, concat);
-		free(concat);
+		last->val = str_freejoin(last->val, append);
+		free(append);
 		if (!last->val)
 			return (lst_free(res, free), false);
 	}
 	return (true);
 }
 
+static bool	cmdp_switch(t_cmd_parseswitch type, char *raw, int *i, int *edge)
+{
+	if (type == CMDP_QUOTE)
+	{
+		if (!chr_isquote(raw[*i]))
+			return (false);
+		(*edge) = chr_quoteclose_idx(raw, raw[*i], *i);
+		if ((*edge) == -1)
+			(*edge) = str_ilen(raw);
+	}
+	cmd_dbgparse(type, raw, *i, *edge);
+	if (type == CMDP_QUOTE)
+		return (true);
+	if (type == CMDP_SPACE)
+		return (ft_isspace(raw[*i]) && !ft_isspace(raw[*i + 1]));
+	if (type == CMDP_TOKEN)
+		return (chr_istoken(raw[*i])
+			&& (ft_isspace(raw[*i + 1]) || !raw[*i + 1] || chr_isquote(raw[*i + 1])));
+	if (type == CMDP_WORD)
+		return (!ft_isspace(raw[*i]) && ((ft_isspace(raw[*i + 1]) || !raw[*i + 1] || chr_isquote(raw[*i + 1])) || chr_istoken(raw[*i + 1])));
+	dbg_printf("[cmd_switch]: Unknown TYPE [%d]!\n", type);
+	return (false);
+}
+
 t_list	*cmd_parse_new(char *raw)
 {
 	t_list		*res;
-	t_list		*last;
 	char		*tmp;
 	int			i;
 	int			edgeidx;
@@ -53,24 +93,16 @@ t_list	*cmd_parse_new(char *raw)
 	dbg_printf(COLOR_RED"[cmd_parse] input: `%s`\n"CR, raw);
 	while (raw && raw[++i])
 	{
-		if (ft_isspace(raw[i]) && !ft_isspace(raw[i + 1]))
+		if (cmdp_switch(CMDP_SPACE, raw, &i, &edgeidx))
 			edgeidx = i + 1;
-		else if (chr_isquote(raw[i]))
+		else if (cmdp_switch(CMDP_QUOTE, raw, &i, &edgeidx))
 		{
-			edgeidx = chr_quoteclose_idx(raw, raw[i], i);
-			dbg_printf("Found a quote {%c} at:\t[%3d] - [%3d]:\t", raw[i], i, edgeidx);
-			print_fromto(raw, i, edgeidx);
-			dbg_printf("\n");
-			if (edgeidx == -1)
-				edgeidx = str_ilen(raw);
 			tmp = my_substr(raw, i, edgeidx);
 			if (!tmp)
 				return (lst_free(&res, free), NULL);
 			if (i != 0 && !ft_isspace(raw[i - 1]) && !chr_istoken(raw[i - 1]))
 			{
-				last = lst_gettail(res);
-				dbg_printf("\tIt's near the char %c\n", raw[i - 1]);
-				if (!concat_arg(&res, tmp))
+				if (!lst_strappend_last(&res, tmp, raw[i - 1]))
 					return (NULL);
 			}
 			else if (!lst_addnew_tail(&res, tmp, NULL))
@@ -83,29 +115,21 @@ t_list	*cmd_parse_new(char *raw)
 			else
 				i = edgeidx;
 		}
-		else if (chr_istoken(raw[i]) && (ft_isspace(raw[i + 1]) || !raw[i + 1] || chr_isquote(raw[i + 1])))
+		else if (cmdp_switch(CMDP_TOKEN, raw, &i, &edgeidx))
 		{
-			dbg_printf("Found unquoted TOKEN at:\t[%3d] - [%3d]:\t", edgeidx, i);
-			print_fromto(raw, edgeidx, i);
-			dbg_printf("\n");
 			tmp = my_substr(raw, edgeidx, i);
-			if (!tmp)
-				return (lst_free(&res, free), NULL);
-			else if (!lst_addnew_tail(&res, tmp, NULL))
+			if (!tmp || !lst_addnew_tail(&res, tmp, NULL))
 				return (lst_free(&res, free), free(tmp), NULL);
+			edgeidx = i + 1;
 		}
-		else if ((!ft_isspace(raw[i]) && !chr_istoken(raw[i])) && (ft_isspace(raw[i + 1]) || !raw[i + 1] || chr_isquote(raw[i + 1]) || chr_istoken(raw[i + 1])))
+		else if (cmdp_switch(CMDP_WORD, raw, &i, &edgeidx))
 		{
-			dbg_printf("Found unquoted WORD at:\t[%3d] - [%3d]:\t", edgeidx, i);
-			print_fromto(raw, edgeidx, i);
-			dbg_printf("\n");
 			tmp = my_substr(raw, edgeidx, i);
 			if (!tmp)
 				return (lst_free(&res, free), NULL);
 			if (i != 0 && edgeidx != 0 && !ft_isspace(raw[edgeidx - 1]) && !chr_istoken(raw[edgeidx - 1]))
 			{
-				dbg_printf("\tIt's near the char %c\n", raw[edgeidx - 1]);
-				if (!concat_arg(&res, tmp))
+				if (!lst_strappend_last(&res, tmp, raw[edgeidx - 1]))
 					return (NULL);
 			}
 			else if (!lst_addnew_tail(&res, tmp, NULL))
