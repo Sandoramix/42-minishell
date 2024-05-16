6adf883bea8d04627242d098861f2343ef85f558
odudniak <odudniak@student.42firenze.it>
2024-05-16 18:40:21 +0200

    [tmp] rimozione di variabili d'ambiente inutili e molto lunghi.

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marboccu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 11:41:06 by marboccu          #+#    #+#             */
-/*   Updated: 2024/05/12 18:36:48 by marboccu         ###   ########.fr       */
+/*   Updated: 2024/05/16 18:38:47 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 int	main(int ac, char **av, char **envp)
 {
 	t_var	mshell;
+	t_list	*args;
 
 	(void)av;
 	mshell = (t_var){0};
@@ -24,7 +25,12 @@ int	main(int ac, char **av, char **envp)
 		return (pf_errcode(ERR_INVALID_ARGC), cleanup(&mshell, true, 1), 1);
 	mshell._main.envp = envp;
 	ms_init(&mshell);
-	//ms_prompt(&mshell);
+	args = cmd_parse_new("unset LS_COLORS TERMINATOR XDG_DATA_DIRS \
+	XDG_SESSION_PATH SESSION_MANAGER GIO_LAUNCHED_DESKTOP_FILE PAGER LESS \
+	SHLVL  LANGUAGE GJS_DEBUG_TOPICS ZSH LOGNAME LANG");
+	expand_and_clear(&mshell, args);
+	ms_unset(&mshell, args);
+	lst_free(&args, free);
 	ms_loop(&mshell);
 	return (cleanup(&mshell, true, 0));
 }
