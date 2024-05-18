ab66d6ae7354ac0f6905105065f4d2be234a1a56
odudniak <odudniak@student.42firenze.it>
2024-05-18 17:18:19 +0200

    (tmp) added cleanup for CTRL+C (just for easier developing)

--- main.c
+++ main.c
@@ -6,13 +6,22 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 11:41:06 by marboccu          #+#    #+#             */
-/*   Updated: 2024/05/18 16:30:20 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/05/18 17:17:44 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
 //#if DEBUG == false
+
+
+// TODO REMOVE ME
+t_var *tmpshell;
+void	safe_exit(int signal)
+{
+	(void)signal;
+	cleanup(tmpshell, true, 130);
+}
 
 int	main(int ac, char **av, char **envp)
 {
@@ -25,6 +34,8 @@ int	main(int ac, char **av, char **envp)
 		return (pf_errcode(ERR_INVALID_ARGC), cleanup(&mshell, true, 1), 1);
 	mshell._main.envp = envp;
 	ms_init(&mshell);
+	signal(SIGINT, safe_exit);
+	tmpshell = &mshell;
 	args = cmd_parse("unset LS_COLORS TERMINATOR XDG_DATA_DIRS \
 	XDG_SESSION_PATH SESSION_MANAGER GIO_LAUNCHED_DESKTOP_FILE PAGER LESS \
 	SHLVL  LANGUAGE GJS_DEBUG_TOPICS ZSH LOGNAME LANG");
