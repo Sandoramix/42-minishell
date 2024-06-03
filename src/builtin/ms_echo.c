c471f82809dca3e83af64dd48f50a3bc4c754e3f
Sandoramix <sandoramix.dev@gmail.com>
2024-06-03 22:32:45 +0200

    [fix] `echo` multiple flags (`-n -nnnn -n ...`) handling

--- src/builtin/ms_echo.c
+++ src/builtin/ms_echo.c
@@ -6,22 +6,69 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 16:22:46 by marboccu          #+#    #+#             */
-/*   Updated: 2024/05/20 09:37:24 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/06/03 22:32:16 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+/**
+ * @brief Function that checks whether the given char `c`
+ * is a valid flag value for `echo` command
+ * @param c character to check
+ * @return `true` if the `c` character is allowed flag, `false` otherwise.
+ * @note the subject mentions that we have only to implement `-n`,
+ * but if needed we can extend the allowed flags (`e` and `E`)
+ * without implementing their features only for the combination of the flags
+ * which are handled in bash (e.g. "echo -enEEnnnn hello" > "hello%")
+ * @example
+ * ```
+ * return (c == 'n' || c == 'e' || c == 'E');
+ * ```
+ */
+static bool	ms_valid_echo_flag(char c)
+{
+	return (c == 'n');
+}
+
+/**
+ * @brief Skip possible -n... flags, because of a weird args like:
+ * `echo -nnn -n -n -n hello world`
+ * @param args pointer to commands's args,
+ * including the command's node ("echo")
+ * @return `true` if the echo command should print a `\n` at the end,
+ * `false` otherwise
+ */
+static bool	skip_flags(t_list **args)
+{
+	t_list	*curr;
+	bool	result;
+
+	result = true;
+	curr = *args;
+	if (curr && str_equals(curr->val, "echo"))
+		curr = curr->next;
+	while (curr && str_startswith(curr->val, "-"))
+	{
+		if (str_every(curr->val + 1, ms_valid_echo_flag))
+			result = false;
+		else
+			break ;
+		curr = curr->next;
+	}
+	*args = curr;
+	return (result);
+}
+
 int	ms_echo(t_var *mshell, t_list *args)
 {
 	t_list		*current;
-	const int	len = lst_size(args);
+	bool		should_nl;
 
 	current = args;
-	if (len > 0)
-		current = current->next;
-	if (len > 1 && str_cmp(args->next->val, "-n") == 0)
-		current = current->next;
+	should_nl = skip_flags(&current);
+	dbg_printf(CGRAY"[ms_echo] should print \\n: %s\n"CR,
+		ft_boolstr(should_nl));
 	while (current != NULL)
 	{
 		ft_printf("%s", (char *)current->val);
@@ -29,7 +76,7 @@ int	ms_echo(t_var *mshell, t_list *args)
 		if (current != NULL)
 			ft_printf(" ");
 	}
-	if (len == 1 || (len > 1 && str_cmp(args->next->val, "-n") != 0))
+	if (should_nl)
 		ft_printf("\n");
 	*mshell->status_code = 0;
 	return (OK);
