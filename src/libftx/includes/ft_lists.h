1539e1ad6cbea1be81def3e0e2fcf3f0f9bbf5c1
odudniak <odudniak@student.42firenze.it>
2024-06-05 10:38:41 +0200

    [refactor] moved `lst_split_bystrval` in libftx

--- src/libftx/includes/ft_lists.h
+++ src/libftx/includes/ft_lists.h
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 15:47:35 by odudniak          #+#    #+#             */
-/*   Updated: 2024/05/31 11:51:55 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/06/05 09:56:51 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 
 //!-------------------------LISTS------------------------------
 
-//! MERGING LISTS WITH DOUBLE LINKED LISTS
 /**
  * @brief Allocates and returns a new list node.
  * @attention Uses: malloc
@@ -74,27 +73,15 @@ t_list		*lst_addnew_tail(t_list **head, void *val, void *key);
 int			lst_size(t_list *head);
 
 /**
- * @brief Frees up all the linked nodes
- * @param head List's head.
- * @param valfree_fn function for freeing the value of the node.
- * @return `NULL`
- */
-t_list		*lst_free(t_list **head, void (*valfree_fn)(void *));
-/**
- * @brief Frees the list's head.
- * @param head List's head.
- * @param valfree_fn function for freeing the value of the node.
- * @return `NULL`
- */
-t_list		*lst_delhead(t_list **head, void (*valfree_fn)(void *));
-
-/**
  * @brief Get the node at requested index.
  * @param head List's head.
  * @param idx index to find.
  * @return a node or `NULL` if index is out of range.
  */
 t_list		*lst_getbyidx(t_list *head, int idx);
+
+//-----------------------------------------------------------------------------
+
 /**
  * @brief Find the first node where the key equals to the one requested.
  * @param head List's head.
@@ -121,6 +108,8 @@ t_list		*lst_findbykey_str(t_list *envlist, char *key);
  */
 int			lst_idxof(t_list *head, void *val, bool (*equal)(void *a, void *b));
 
+//-----------------------------------------------------------------------------
+
 /**
  * @brief Print the given list by assuming that each val & key are of `int` type.
  * @param head List's head.
@@ -135,6 +124,23 @@ void		*lst_printint(t_list *head);
  */
 void		*lst_printstr(t_list *head);
 
+//-----------------------------------------------------------------------------
+
+/**
+ * @brief Frees up all the linked nodes
+ * @param head List's head.
+ * @param valfree_fn function for freeing the value of the node.
+ * @return `NULL`
+ */
+t_list		*lst_free(t_list **head, void (*valfree_fn)(void *));
+/**
+ * @brief Frees the list's head.
+ * @param head List's head.
+ * @param valfree_fn function for freeing the value of the node.
+ * @return `NULL`
+ */
+t_list		*lst_delhead(t_list **head, void (*valfree_fn)(void *));
+
 /**
  * @brief Delete all nodes of the given list by a given `key`.
  * @param lst pointer to the list's head.
@@ -145,7 +151,31 @@ void		*lst_printstr(t_list *head);
 t_list		*lst_delbykey(t_list **lst, void *key,
 				bool (*equal)(void *a, void *b), void (*valfree_fn)(void *));
 
+/**
+ * @brief Deletes the given pointer to a list node,
+ * frees it's content and set's the pointer to `NULL`
+ * @param node pointer of the node to deallocate.
+ * @param valfree_fn function that frees the content. All other pointers will be
+ * freed with `free`
+ * @return `NULL`
+ * @attention It doesn't handle any list recovery, so you must be sure that this
+ * node is alone and wont break an existing list after being deleted.
+ */
 t_list		*lst_delnode(t_list **node, void (*valfree_fn)(void *));
+
+//-----------------------------------------------------------------------------
+
+/**
+ * @brief Split a list by the given node's value.
+ * @attention The node with exact value `val` (`node->val`) will be deleted.
+ * if there are two consecutive nodes with the value `val` then an'empty list
+ * will be created between those two (`NULL` value).
+ * @param all List to split
+ * @param val value to split the list with.
+ * @return `t_list` where each node is a `t_list` returned by the split.
+ */
+t_list		*lst_split_bystrval(t_list *all, char *val);
+
 //-----------------------------------------------------------------------------
 
 #endif
