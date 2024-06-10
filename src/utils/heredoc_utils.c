437f6d3bef3208baefab3d10bb342a01e2ba3e56
marboccu <marboccu@student.42fi.it>
2024-06-10 18:01:41 +0200

    si

--- src/utils/heredoc_utils.c
+++ src/utils/heredoc_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
+/*   By: marboccu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 10:36:40 by marboccu          #+#    #+#             */
-/*   Updated: 2024/06/09 14:24:41 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/06/10 17:59:44 by marboccu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,11 @@ char	*gen_heredocs(int count)
 	char	*heredoc_file;
 
 	suffix = ft_itoa(count);
-	heredoc_file = file_gen_name(".heredoc_", R_OK | W_OK);
-	prefixed_name = str_join(heredoc_file, suffix);
-	if (!prefixed_name)
-		return (ft_perror("file_gen_name"), NULL);
+	prefixed_name = str_join(".heredoc_", suffix);
 	free(suffix);
-	free(heredoc_file);
-	return (prefixed_name);
+	heredoc_file = file_gen_name(prefixed_name, R_OK | W_OK);
+	free(prefixed_name);
+	if (!heredoc_file)
+		return (pf_errcode(ERR_MALLOC), NULL);
+	return (heredoc_file);
 }
